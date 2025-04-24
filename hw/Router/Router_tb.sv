d6edb66c3cadb17a01c3711c757bfe3e17f20c84
Thodorhs <thodp@ics.forth.gr>
2025-04-24 17:38:51 +0300

    [fix] Fix mistakes in NOC mesh generation

--- hw/Router/Router_tb.sv
+++ hw/Router/Router_tb.sv
@@ -27,107 +27,102 @@ import router_pkg::*;
     logic clk = 1;
     logic reset = 0;
     logic start = 0;
-    
+
+    FLIT_t data_out   [ROWS][COLUMNS][NUM_OF_PORTS];
+    logic transmit    [ROWS][COLUMNS][NUM_OF_PORTS];
+    logic send        [ROWS][COLUMNS][NUM_OF_PORTS];
     logic downstream_ack [ROWS][COLUMNS][NUM_OF_PORTS];
     logic downstream_req [ROWS][COLUMNS][NUM_OF_PORTS];
-    logic down_to_upstream_req[ROWS][COLUMNS][NUM_OF_PORTS] = '{default:'0};
-    logic down_to_upstream_ack[ROWS][COLUMNS][NUM_OF_PORTS] = '{default:'0};
-    router_pipeline_bus_t s2d [ROWS][COLUMNS][NUM_OF_PORTS] = '{default:'0}; // switch to downstream
+    router_pipeline_bus_t s2d [ROWS][COLUMNS][NUM_OF_PORTS];
     FLIT_t to_router [ROWS][COLUMNS][NUM_OF_PORTS];
     
+    always #(`CLK_PERIOD) clk = ~clk;
     
-    always# (`CLK_PERIOD) clk = ~clk;
-    
-    integer i,j;
-    always_comb begin
-        to_router          = '{default:'0};
-        downstream_req     = '{default:'0};
-        downstream_ack     = '{default:'0};
-        for (i = 0; i < ROWS; i++) begin
-            for (j = 0; j < COLUMNS; j++) begin
-                //connect LOCAL cables to traffic gen
-                to_router[i][j][LOCAL_PORT] = s2d[i][j][LOCAL_PORT].flit;
-                downstream_ack[i][j][LOCAL_PORT] = down_to_upstream_ack[i][j][LOCAL_PORT];
-                downstream_req[i][j][LOCAL_PORT] = down_to_upstream_req[i][j][LOCAL_PORT];
-                
-                // NORTH neighbor
-                if (i > 0) begin
-                    to_router[i-1][j][SOUTH_PORT] = s2d[i][j][NORTH_PORT].flit;
-                    downstream_req[i-1][j][SOUTH_PORT] = down_to_upstream_req[i][j][NORTH_PORT];
-                    downstream_ack[i-1][j][SOUTH_PORT] = down_to_upstream_ack[i][j][NORTH_PORT];
-                end
-    
-                // SOUTH neighbor
-                if (i < ROWS-1) begin
-                    to_router[i+1][j][NORTH_PORT] = s2d[i][j][SOUTH_PORT].flit;
-                    downstream_req[i+1][j][NORTH_PORT] = down_to_upstream_req[i][j][SOUTH_PORT];
-                    downstream_ack[i+1][j][NORTH_PORT] = down_to_upstream_ack[i][j][SOUTH_PORT];
-                end
-    
-                // WEST neighbor
-                if (j > 0) begin
-                    to_router[i][j-1][EAST_PORT] = s2d[i][j][WEST_PORT].flit;
-                    downstream_req[i][j-1][EAST_PORT] = down_to_upstream_req[i][j][WEST_PORT];
-                    downstream_ack[i][j-1][EAST_PORT] = down_to_upstream_ack[i][j][WEST_PORT];
-                end
-    
-                // EAST neighbor
-                if (j < COLUMNS-1) begin
-                    to_router[i][j+1][WEST_PORT] = s2d[i][j][EAST_PORT].flit;
-                    downstream_req[i][j+1][WEST_PORT] = down_to_upstream_req[i][j][EAST_PORT];
-                    downstream_ack[i][j+1][WEST_PORT] = down_to_upstream_ack[i][j][EAST_PORT];
-                end
-            end
-        end
-    end
-    
-    genvar k,l;
+    genvar i, j;
     generate
-        for (k = 0; k < ROWS; k++) begin
-            for (l = 0; l < COLUMNS; l++) begin
-                 TrafficGenerator  trafficGen (
+        for (i = 0; i < ROWS; i++) begin: row_gen
+            for (j = 0; j < COLUMNS; j++) begin: col_gen
+                TrafficGenerator trafficGen (
                     .clk(clk),
                     .reset_n(reset),
                     .i_start(start),
-                    .i_send(downstream_ack[k][l][LOCAL_PORT]),
-                    .o_flit(s2d[k][l][LOCAL_PORT]),
-                    .o_transmit(down_to_upstream_req[k][l][LOCAL_PORT])
-                 );
-                 
-                 Router #(
-                .router_conf('{xaddr: k, yaddr: l})
-                )router(
+                    .i_send(send[i][j][LOCAL_PORT]),
+                    .o_flit(data_out[i][j][LOCAL_PORT]),
+                    .o_transmit(transmit[i][j][LOCAL_PORT])
+                );
+                
+                Router #(
+                    .router_conf('{xaddr: i, yaddr: j})
+                ) router (
                     .clk(clk),
                     .reset_n(reset),
-                    .i_flit(to_router[k][l]),
-                    .i_upstream_req(downstream_req[k][l]),
-                    .i_downstream_ack(downstream_ack[k][l]),
-                    .o_on_off(down_to_upstream_ack[k][l]),
-                    .o_downstream_req(down_to_upstream_req[k][l]),
-                    .o_s2d(s2d[k][l])
+                    .i_flit(to_router[i][j]),
+                    .i_upstream_req(transmit[i][j]),
+                    .i_downstream_ack(downstream_ack[i][j]),
+                    .o_on_off(send[i][j]),
+                    .o_downstream_req(downstream_req[i][j]),
+                    .o_s2d(s2d[i][j])
                 );
-                 
+                
+                // Connect LOCAL
+                assign to_router[i][j][LOCAL_PORT] = data_out[i][j][LOCAL_PORT];
+                
+                // North port connection
+                if (i > 0) begin: north_connect
+                    assign to_router[i][j][NORTH_PORT] = s2d[i-1][j][SOUTH_PORT].flit;
+                    assign transmit[i][j][NORTH_PORT] = downstream_req[i-1][j][SOUTH_PORT];
+                    assign downstream_ack[i-1][j][SOUTH_PORT] = send[i][j][NORTH_PORT];
+                end else begin: north_boundary
+                    assign to_router[i][j][NORTH_PORT] = '0;
+                    assign transmit[i][j][NORTH_PORT] = '0;
+                end
+                
+                // South port connection
+                if (i < ROWS-1) begin: south_connect
+                    assign to_router[i][j][SOUTH_PORT] = s2d[i+1][j][NORTH_PORT].flit;
+                    assign transmit[i][j][SOUTH_PORT] = downstream_req[i+1][j][NORTH_PORT];
+                    assign downstream_ack[i+1][j][NORTH_PORT] = send[i][j][SOUTH_PORT];
+                end else begin: south_boundary
+                    assign to_router[i][j][SOUTH_PORT] = '0;
+                    assign transmit[i][j][SOUTH_PORT] = '0;
+                end
+                
+                // West port connection
+                if (j > 0) begin: west_connect
+                    assign to_router[i][j][WEST_PORT] = s2d[i][j-1][EAST_PORT].flit;
+                    assign transmit[i][j][WEST_PORT] = downstream_req[i][j-1][EAST_PORT];
+                    assign downstream_ack[i][j-1][EAST_PORT] = send[i][j][WEST_PORT];
+                end else begin: west_boundary
+                    assign to_router[i][j][WEST_PORT] = '0;
+                    assign transmit[i][j][WEST_PORT] = '0;
+                end
+                
+                // East port connection
+                if (j < COLUMNS-1) begin: east_connect
+                    assign to_router[i][j][EAST_PORT] = s2d[i][j+1][WEST_PORT].flit;
+                    assign transmit[i][j][EAST_PORT] = downstream_req[i][j+1][WEST_PORT];
+                    assign downstream_ack[i][j+1][WEST_PORT] = send[i][j][EAST_PORT];
+                end else begin: east_boundary
+                    assign to_router[i][j][EAST_PORT] = '0;
+                    assign transmit[i][j][EAST_PORT] = '0;
+                end
             end
         end
-    endgenerate 
+    endgenerate
     
     initial begin
-      
         @(posedge clk);
-
         reset = 1;
         start = 1;
         
         repeat (5) begin 
             @(posedge clk);
-         end
-       // start = 0;
-        //send = 1;
-         repeat (50) begin 
+        end
+        
+        repeat (50) begin 
             @(posedge clk);
-         end
-        // start= 0;
-         //send = 0;
+        end
+        
         $finish;
     end
 endmodule
